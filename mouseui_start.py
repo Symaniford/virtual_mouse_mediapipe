e5ed8fe5e7469c646e74685946e1ea9c7da7e11d
ymzowl <77684194+ymzowl@users.noreply.github.com>
2022-06-22 09:55:20 +0800

    Add files via upload

---
+++ mouseui_start.py
@@ -0,0 +1,19 @@
+import sys
+import mouseui
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+
+
+    app = QApplication(sys.argv)
+
+    mainWindow = QMainWindow()
+
+    ui = mouseui.Ui_MainWindow()
+
+    ui.setupUi(mainWindow)
+
+    mainWindow.show()
+
+    sys.exit(app.exec_())
